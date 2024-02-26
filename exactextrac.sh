4db5beed070f7811e46bdb89b1c9e72e9edee119
Shervan Gharari <28273583+ShervanGharari@users.noreply.github.com>
2024-02-26 10:25:59 -0600

    Update exactextrac.sh

--- exactextrac.sh
+++ exactextrac.sh
@@ -1,13 +1,28 @@
 ```
 cd ~/ # cd to home or the directory to build the sif file. 
-export APPTAINER_CACHEDIR=$(pwd) # currecnt directory
 rm -rf *.sif cache/
 apptainer pull docker://isciences/exactextract
-apptainer run exactextract_latest.sif -r "/home/shg096/GRASS_APPTAINER_TEST/output/filled_zones.tif" -p /home/shg096/GRASS_APPTAINER_TEST/output/subbasins.gpkg.shp -s "frac" -o ~/output.csv
+apptainer run exactextract_latest.sif \
+  -r "DEM:/home/shg096/GRASS_APPTAINER_TEST/output/filled.tif" \
+  -p /home/shg096/GRASS_APPTAINER_TEST/output/subbasins.gpkg \
+  -f cat \
+  -s "mean(DEM)" \
+  -o ~/output.csv
 ```
 
-cd ~/ # cd to home or the directory to build the sif file. 
-export APPTAINER_CACHEDIR=$(pwd) # currecnt directory
-rm -rf *.sif cache/
-apptainer pull docker://isciences/exactextract-build-env
-apptainer run exactextract-build-env_latest.sif
+compilation
+```
+cd
+module purge
+module reset
+module load StdEnv/2020 gcc/9.3.0 geos gdal cmake
+
+cd
+rm -rf exactextract
+git clone https://github.com/isciences/exactextract.git --branch v0.1.0
+cd exactextract/
+mkdir cmake-build-release
+cd cmake-build-release
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+```
